8f3167c961011f98a570a175cf64f728802db044
ankitnathtiwari <ankitfounder@gmail.com>
2021-08-04 19:02:22 +0530

    chnaged ui, date and order

--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -9,6 +9,7 @@ export const posts1 = [
       "हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है। हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है।",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 2,
@@ -18,6 +19,7 @@ export const posts1 = [
       "हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है। हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है।",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 3,
@@ -27,6 +29,7 @@ export const posts1 = [
       "हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है।",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 4,
@@ -36,6 +39,7 @@ export const posts1 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 5,
@@ -45,8 +49,10 @@ export const posts1 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
 ];
+
 export const posts2 = [
   {
     _id: 6,
@@ -56,6 +62,7 @@ export const posts2 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 7,
@@ -65,6 +72,7 @@ export const posts2 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 8,
@@ -74,6 +82,7 @@ export const posts2 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 9,
@@ -83,6 +92,7 @@ export const posts2 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 10,
@@ -92,8 +102,10 @@ export const posts2 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
 ];
+
 export const posts3 = [
   {
     _id: 11,
@@ -103,6 +115,7 @@ export const posts3 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 12,
@@ -112,6 +125,7 @@ export const posts3 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 13,
@@ -121,6 +135,7 @@ export const posts3 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 14,
@@ -130,6 +145,7 @@ export const posts3 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
   {
     _id: 15,
@@ -139,5 +155,6 @@ export const posts3 = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     image: testImage,
     author: "60f12e1f809f4c42b89c7c08",
+    pub_date: "2021-07-30T07:15:48.023+00:00",
   },
 ];
--- src/post/index.js
+++ src/post/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
+import moment from "moment";
 
 export const Post = ({ post }) => {
   const testImage = "http://localhost:8000/image/fakeimage.jpg";
@@ -9,6 +10,9 @@ export const Post = ({ post }) => {
       <img src={post.image} alt='Girl in a jacket' width='200' height='200' />
       <h2>{post.title}</h2>
       <p className='post-para'>{post.content}</p>
+      <p className='post_date'>
+        At {moment(post.pub_date).format(" h:mm:ss, on Mo MMM YYYY ")}
+      </p>
     </div>
   );
 };
