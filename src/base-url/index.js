876d97df606d8ec02bf3034761df2a0acc41e6eb
ankitnathtiwari <ankitfounder@gmail.com>
2021-12-16 22:40:05 +0530

    new build and base url change

--- src/base-url/index.js
+++ src/base-url/index.js
@@ -8,7 +8,7 @@ export const stagingUrl = "http://3.110.33.147";
 export const prodReporterUrl = "https://reporters.newsbird.live";
 export const reporterDomainStaging = "3.110.33.147";
 
-export const mainUrl = stagingUrl;
+export const mainUrl = prodSecureUrl;
 export const shareUrl = mainUrl;
 export const baseUrl = `${mainUrl}/json_api/post/allpost`;
 
