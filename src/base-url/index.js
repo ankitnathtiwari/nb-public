b8adaa46ceb0689ec950ccf9171d25252e5e7993
ankitnathtiwari <ankitfounder@gmail.com>
2022-01-01 11:22:32 +0530

    test with ip

--- src/base-url/index.js
+++ src/base-url/index.js
@@ -1,4 +1,4 @@
-export const awsIpUrl = "http://3.17.55.187";
+export const prodIp = "http://15.207.52.81";
 export const s3imageUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com";
 export const localApiUrl = "http://localhost:8000";
 export const mockUrl = "http://localhost:8080";
@@ -8,7 +8,7 @@ export const stagingUrl = "http://3.110.33.147";
 export const prodReporterUrl = "https://reporters.newsbird.live";
 export const reporterDomainStaging = "3.110.33.147";
 
-export const mainUrl = prodSecureUrl;
+export const mainUrl = prodIp;
 export const shareUrl = mainUrl;
 export const baseUrl = `${mainUrl}/json_api/post/allpost`;
 
