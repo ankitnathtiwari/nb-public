71fd270a18081abf63eb24c62922d1d776bda199
ankitnathtiwari <ankitfounder@gmail.com>
2022-01-18 23:46:10 +0530

    commited

--- src/base-url/index.js
+++ src/base-url/index.js
@@ -1,5 +1,4 @@
 export const awsIpUrl = "http://3.17.55.187";
-export const s3imageUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com";
 export const localApiUrl = "http://localhost:8000";
 export const mockUrl = "http://localhost:8080";
 export const prodSecureUrl = "https://newsbird.live";
@@ -7,13 +6,23 @@ export const prodUrl = "http://newsbird.live";
 export const stagingUrl = "http://65.0.18.242";
 export const prodReporterUrl = "https://reporters.newsbird.live";
 export const reporterDomainStaging = "3.110.33.147";
-export const videoUrl = "https://nb-test-video-mum.s3.ap-south-1.amazonaws.com";
 
-export const mainUrl = stagingUrl;
+export const stagingVideoUrl = "https://nb-test-video-mum.s3.ap-south-1.amazonaws.com";
+export const stagingImageUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com";
+export const stagingThumbnailUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com";
+
+export const prodVideoUrl = "https://nb-video-post-mum.s3.ap-south-1.amazonaws.com";
+export const prodImageUrl = "https://nb-post-image-mum.s3.ap-south-1.amazonaws.com";
+export const prodthumbnailUrl = "https://nb-video-thumbnail-mum.s3.ap-south-1.amazonaws.com";
+
+export const videoUrl = prodVideoUrl;
+export const s3imageUrl = prodImageUrl;
+
+export const mainUrl = prodSecureUrl;
 export const shareUrl = mainUrl;
 export const baseUrl = `${mainUrl}/json_api`;
 export const imageUrl = s3imageUrl;
-export const thumbnailUrl = s3imageUrl;
+export const thumbnailUrl = prodthumbnailUrl;
 
 export const topics = [
   "",
