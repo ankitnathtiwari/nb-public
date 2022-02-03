39842754dc3b10d4dda72fab06ce355bad393487
ankitnathtiwari <ankitfounder@gmail.com>
2022-02-03 16:59:06 +0530

    added post thumbnail

--- src/social-share/index.js
+++ src/social-share/index.js
@@ -38,10 +38,10 @@ export const SocialShare = ({ post, shareUrl }) => {
           property="og:description"
           content="How much does culture influence creative thinking?"
         />
-        {/* <meta
+        <meta
           property="og:image"
-          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
-        /> */}
+          content={`${appConfig.url.thumbnail}/${post.thumbNail}`}
+        />
       </Helmet>
 
       <div>
