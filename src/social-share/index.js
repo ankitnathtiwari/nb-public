8b9a0f94dcb9eed02e91fbeedd72440580e78783
ankitnathtiwari <ankitfounder@gmail.com>
2022-02-03 16:42:30 +0530

    added helment

--- src/social-share/index.js
+++ src/social-share/index.js
@@ -20,7 +20,28 @@ export const SocialShare = ({ post, shareUrl }) => {
   return (
     <div className="social-share">
       <Helmet>
-        <meta name="description" content="Helmet application" />
+        <meta
+          property="og:url"
+          content="https://www.youtube.com/watch?v=BQBjVr1iHH4"
+        />
+        <title>{post.title}</title>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
       </Helmet>
 
       <div>
