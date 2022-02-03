50f5770dd737e13af21a711847fb9f6875481fcc
ankitnathtiwari <ankitfounder@gmail.com>
2022-02-03 17:17:58 +0530

    fsdfs

--- src/social-share/index.js
+++ src/social-share/index.js
@@ -36,7 +36,7 @@ export const SocialShare = ({ post, shareUrl }) => {
         />
         <meta
           property="og:image"
-          content={`http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg`}
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
         />
       </Helmet>
 
