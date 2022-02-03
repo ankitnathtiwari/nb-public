69a29a4c05ba0d5416a156101390b0a46bfc7380
ankitnathtiwari <ankitfounder@gmail.com>
2022-02-03 17:29:59 +0530

    sfsf

--- src/social-share/index.js
+++ src/social-share/index.js
@@ -32,7 +32,7 @@ export const SocialShare = ({ post, shareUrl }) => {
           content="How much does culture influence creative thinking?"
         />
         <meta
-          property="og:image"s
+          property="og:image"
           content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
         />
       </Helmet>
