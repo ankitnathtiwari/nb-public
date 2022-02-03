bb1428f9587df528ed1922553400347b4cdfd2e9
ankitnathtiwari <ankitfounder@gmail.com>
2022-02-03 17:33:10 +0530

    sf

--- src/social-share/index.js
+++ src/social-share/index.js
@@ -33,7 +33,7 @@ export const SocialShare = ({ post, shareUrl }) => {
         />
         <meta
           property="og:image"
-          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+          content={`${appConfig.url.thumbnail}/${post.thumbNail}`}
         />
       </Helmet>
 
